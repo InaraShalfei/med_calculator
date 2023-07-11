3bff5e4c081180472d16952097853eb09a388a78
inara <inarashugaikina@gmail.com>
2023-07-11 15:06:50 +0500

    Register project models in admin panel

--- medical_calculator/calculator/admin.py
+++ medical_calculator/calculator/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import NormalParameter, Examination
+
+
+class NormalParameterAdmin(admin.ModelAdmin):
+    list_display = ('name', 'title', 'measure_unit', 'low_limit', 'high_limit')
+    empty_value_display = '-пусто-'
+
+
+class ExaminationAdmin(admin.ModelAdmin):
+    list_display = ('full_name', 'diagnosis', 'diagnosis_date', 'RBC', 'HGB', 'HCT', 'MCV', 'MCH', 'MCHC', 'RDW_CV', 'RDW_SD',
+                    'ferritin', 'transferrin', 'TIBC', 'fe', 'B9', 'B12', 'total_bilirubin', 'LDH')
+    empty_value_display = '-пусто-'
+
+
+admin.site.register(NormalParameter, NormalParameterAdmin)
+admin.site.register(Examination, ExaminationAdmin)
