8eba5cfe182df4d0f259778ea564cf86e8aa9409
inara <inarashugaikina@gmail.com>
2023-07-11 16:30:06 +0500

    Make possible for fields in model to be null or blank

--- medical_calculator/calculator/models.py
+++ medical_calculator/calculator/models.py
@@ -1,4 +1,7 @@
+import datetime
+
 from django.db import models
+from django.contrib import admin
 
 
 class NormalParameter(models.Model):
@@ -20,22 +23,22 @@ class Examination(models.Model):
     full_name = models.CharField(max_length=250, verbose_name='ФИО пациента')
     diagnosis = models.CharField(max_length=250, blank=True, null=True, verbose_name='диагноз')
     diagnosis_date = models.DateField(auto_now=True, verbose_name='дата диагноза')
-    RBC = models.FloatField(verbose_name='уровень эритроцитов')
-    HGB = models.FloatField(verbose_name='уровень гемоглобина')
-    HCT = models.FloatField(verbose_name='гематокрит')
-    MCV = models.FloatField(verbose_name='средн. объем эритроцита')
-    MCH = models.FloatField(verbose_name='средн. эритроцитарный гемоглобин')
-    MCHC = models.FloatField(verbose_name='средн. концентрация гемоглобина в эритроците')
-    RDW_CV = models.FloatField(verbose_name='распределение эритроцитов по объёму, КВ')
-    RDW_SD = models.FloatField(verbose_name='распределение эритроцитов по объёму, СКВ')
-    ferritin = models.FloatField(verbose_name='ферритин')
-    transferrin = models.FloatField(verbose_name='трансферрин')
-    TIBC = models.FloatField(verbose_name='TIBC')
-    fe = models.FloatField(verbose_name='железо')
-    B9 = models.FloatField(verbose_name='витамин B9')
-    B12 = models.FloatField(verbose_name='витамин B12')
-    total_bilirubin = models.FloatField(verbose_name='общий билирубин')
-    LDH = models.FloatField(verbose_name='ЛДГ')
+    RBC = models.FloatField(verbose_name='уровень эритроцитов', blank=True, null=True)
+    HGB = models.FloatField(verbose_name='уровень гемоглобина', blank=True, null=True)
+    HCT = models.FloatField(verbose_name='гематокрит', blank=True, null=True)
+    MCV = models.FloatField(verbose_name='средн. объем эритроцита', blank=True, null=True)
+    MCH = models.FloatField(verbose_name='средн. эритроцитарный гемоглобин', blank=True, null=True)
+    MCHC = models.FloatField(verbose_name='средн. концентрация гемоглобина в эритроците', blank=True, null=True)
+    RDW_CV = models.FloatField(verbose_name='распределение эритроцитов по объёму, КВ', blank=True, null=True)
+    RDW_SD = models.FloatField(verbose_name='распределение эритроцитов по объёму, СКВ', blank=True, null=True)
+    ferritin = models.FloatField(verbose_name='ферритин', blank=True, null=True)
+    transferrin = models.FloatField(verbose_name='трансферрин', blank=True, null=True)
+    TIBC = models.FloatField(verbose_name='TIBC', blank=True, null=True)
+    fe = models.FloatField(verbose_name='железо', blank=True, null=True)
+    B9 = models.FloatField(verbose_name='витамин B9', blank=True, null=True)
+    B12 = models.FloatField(verbose_name='витамин B12', blank=True, null=True)
+    total_bilirubin = models.FloatField(verbose_name='общий билирубин', blank=True, null=True)
+    LDH = models.FloatField(verbose_name='ЛДГ', blank=True, null=True)
 
     class Meta:
         verbose_name = 'Анализ'
@@ -43,3 +46,9 @@ class Examination(models.Model):
 
     def __str__(self):
         return self.full_name
+
+    @admin.display(description='Дата диагноза')
+    def readable_date(self):
+        date = datetime.datetime.strptime(str(self.diagnosis_date), '%Y-%m-%d')
+        return datetime.datetime.strftime(date, '%d.%m.%Y')
+
