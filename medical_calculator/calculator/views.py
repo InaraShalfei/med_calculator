626ae115efaf87b918f3f314e8c81ce56c6029a1
inara <inarashugaikina@gmail.com>
2023-07-11 16:30:36 +0500

    Create initial function to handle request data

--- medical_calculator/calculator/views.py
+++ medical_calculator/calculator/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponse
+
+from .models import Examination
+
+
+@csrf_exempt
+def handle_results(request):
+    request_data = json.loads(request.body.decode('utf-8'))
+    patient, created = Examination.objects.get_or_create(full_name=request_data['full_name'], RBC=request_data['RBC'],
+                                                         HGB=request_data['HGB'], HCT=request_data['HCT'],
+                                                         MCV=request_data['MCV'], MCH=request_data['MCH'],
+                                                         MCHC=request_data['MCHC'], RDW_CV=request_data['RDW_CV'],
+                                                         RDW_SD=request_data['RDW_SD'], ferritin=request_data['ferritin'],
+                                                         transferrin=request_data['transferrin'], TIBC=request_data['TIBC'],
+                                                         fe=request_data['fe'], B9=request_data['B9'], B12=request_data['B12'],
+                                                         total_bilirubin=request_data['total_bilirubin'],
+                                                         LDH=request_data['LDH'])
+    print(json.loads(request.body.decode('utf-8'))['full_name'])
+
+    return HttpResponse()
+
+
+
+
