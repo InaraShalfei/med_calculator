b544d9d1c28aa1673ccf5fcd749f3ac5a97ecf64
inara <inarashugaikina@gmail.com>
2023-07-12 13:59:48 +0500

    Add checking if value is None while creating examination object

--- medical_calculator/calculator/views.py
+++ medical_calculator/calculator/views.py
@@ -24,7 +24,7 @@ def get_normatives():
         'ferritin': NormalParameter.objects.get(name='ferritin'),
         'fe': NormalParameter.objects.get(name='fe'),
         'В9': NormalParameter.objects.get(name='В9'),
-        'В12': NormalParameter.objects.get(name='В12'),
+        'B12': NormalParameter.objects.get(name='B12'),
         'total_bilirubin': NormalParameter.objects.get(name='total_bilirubin'),
         'LDH': NormalParameter.objects.get(name='LDH'),
         'homocystein': NormalParameter.objects.get(name='homocystein'),
@@ -126,60 +126,92 @@ def is_anemia_B12(examination: Examination) -> bool:
         return True
 
 
-def get_diagnosises(examination:Examination) -> List:
+def is_autoimmune_anemia(examination: Examination) -> bool:
+    normatives = get_normatives()
+    if (normatives['RBC'].is_low(examination.RBC) and
+            normatives['HGB'].is_low(examination.HGB) and
+            normatives['HCT'].is_normal_or_low(examination.HCT) and
+            normatives['MCV'].is_normal_or_high(examination.MCV) and
+            normatives['MCH'].is_normal_or_low(examination.MCH) and
+            normatives['МСНС'].is_normal_or_high(examination.MCHC) and
+            normatives['ferritin'].is_normal_or_high(examination.ferritin) and
+            normatives['fe'].is_normal_or_high(examination.fe) and
+            normatives['transferrin'].is_normal_or_high(examination.transferrin) and
+            normatives['TIBC'].is_high(examination.TIBC) and
+            normatives['total_bilirubin'].is_high(examination.total_bilirubin) and
+    #TODO Прямая реакция  Кумбса
+            normatives['LDH'].is_high(examination.LDH)):
+        return True
+
+
+def get_diagnoses(examination:Examination) -> List:
     diagnoses_callbacks = {
         is_anemia_1: '1 степень железодефицитной анемии',
         is_anemia_2: '2 степень железодефицитной анемии',
         is_anemia_3: '3 степень железодефицитной анемии',
         is_anemia_B9: 'В9 дефицитная анемия',
-        is_anemia_B12: 'В12 дефицитная анемия'
+        is_anemia_B12: 'В12 дефицитная анемия',
+        is_autoimmune_anemia: 'Аутоиммунная гемолитическая анемия '
     }
 
     diagnoses = []
-    for func, diagnosys in diagnoses_callbacks.items():
+    for func, diagnosis in diagnoses_callbacks.items():
         if func(examination):
-            diagnoses.append(diagnosys)
+            diagnoses.append(diagnosis)
     return diagnoses
 
 @csrf_exempt
 def handle_results(request):
     request_data = json.loads(request.body.decode('utf-8'))
-    if Examination.objects.filter(
-            Q(full_name=request_data['full_name']) & Q(diagnosis_date=datetime.datetime.today())).exists():
-        examination = Examination.objects.filter(Q(full_name=request_data['full_name']) &
-                                   Q(diagnosis_date=datetime.datetime.today())).update(RBC=request_data['RBC'],
-                                                                                       HGB=request_data['HGB'],
-                                                                                       HCT=request_data['HCT'],
-                                                                                       MCV=request_data['MCV'],
-                                                                                       MCH=request_data['MCH'],
-                                                                                       MCHC=request_data['MCHC'],
-                                                                                       RDW_CV=request_data['RDW_CV'],
-                                                                                       RDW_SD=request_data['RDW_SD'],
-                                                                                       ferritin=request_data[
-                                                                                           'ferritin'],
-                                                                                       transferrin=request_data[
-                                                                                           'transferrin'],
-                                                                                       TIBC=request_data['TIBC'],
-                                                                                       fe=request_data['fe'],
-                                                                                       B9=request_data['B9'],
-                                                                                       B12=request_data['B12'],
-                                                                                       total_bilirubin=request_data[
-                                                                                           'total_bilirubin'],
-                                                                                       LDH=request_data['LDH'])
+    existing_examination_condition = Q(full_name=request_data['full_name']) & Q(diagnosis_date=datetime.datetime.today())
+    if Examination.objects.filter(existing_examination_condition).exists():
+        Examination.objects.filter(existing_examination_condition).update(
+            RBC=request_data['RBC'] if 'RBC' in request_data else None,
+            HGB=request_data['HGB'] if 'HGB' in request_data else None,
+            HCT=request_data['HCT'] if 'HCT' in request_data else None,
+            MCV=request_data['MCV'] if 'MCV' in request_data else None,
+            MCH=request_data['MCH'] if 'MCH' in request_data else None,
+            MCHC=request_data['MCHC'] if 'MCHC' in request_data else None,
+            RDW_CV=request_data['RDW_CV'] if 'RDW_CV' in request_data else None,
+            RDW_SD=request_data['RDW_SD'] if 'RDW_SD' in request_data else None,
+            ferritin=request_data['ferritin'] if 'ferritin' in request_data else None,
+            transferrin=request_data['transferrin'] if 'transferrin' in request_data else None,
+            TIBC=request_data['TIBC'] if 'TIBC' in request_data else None,
+            fe=request_data['fe'] if 'fe' in request_data else None,
+            B9=request_data['B9'] if 'B9' in request_data else None,
+            B12=request_data['B12'] if 'B12' in request_data else None,
+            total_bilirubin=request_data['total_bilirubin'] if 'total_bilirubin' in request_data else None,
+            LDH=request_data['LDH'] if 'LDH' in request_data else None,
+            homocystein=request_data['homocystein'] if 'homocystein' in request_data else None
+        )
+        examination = Examination.objects.filter(existing_examination_condition).get()
     else:
-        examination = Examination.objects.create(full_name=request_data['full_name'], RBC=request_data['RBC'],
-                                   HGB=request_data['HGB'], HCT=request_data['HCT'],
-                                   MCV=request_data['MCV'], MCH=request_data['MCH'],
-                                   MCHC=request_data['MCHC'], RDW_CV=request_data['RDW_CV'],
-                                   RDW_SD=request_data['RDW_SD'], ferritin=request_data['ferritin'],
-                                   transferrin=request_data['transferrin'], TIBC=request_data['TIBC'],
-                                   fe=request_data['fe'], B9=request_data['B9'], B12=request_data['B12'],
-                                   total_bilirubin=request_data['total_bilirubin'],
-                                   LDH=request_data['LDH'])
+        examination = Examination.objects.create(
+            full_name=request_data['full_name'],
+            RBC=request_data['RBC'] if 'RBC' in request_data else None,
+            HGB=request_data['HGB'] if 'HGB' in request_data else None,
+            HCT=request_data['HCT'] if 'HCT' in request_data else None,
+            MCV=request_data['MCV'] if 'MCV' in request_data else None,
+            MCH=request_data['MCH'] if 'MCH' in request_data else None,
+            MCHC=request_data['MCHC'] if 'MCHC' in request_data else None,
+            RDW_CV=request_data['RDW_CV'] if 'RDW_CV' in request_data else None,
+            RDW_SD=request_data['RDW_SD'] if 'RDW_SD' in request_data else None,
+            ferritin=request_data['ferritin'] if 'ferritin' in request_data else None,
+            transferrin=request_data['transferrin'] if 'transferrin' in request_data else None,
+            TIBC=request_data['TIBC'] if 'TIBC' in request_data else None,
+            fe=request_data['fe'] if 'fe' in request_data else None,
+            B9=request_data['B9'] if 'B9' in request_data else None,
+            B12=request_data['B12'] if 'B12' in request_data else None,
+            total_bilirubin=request_data['total_bilirubin'] if 'total_bilirubin' in request_data else None,
+            LDH=request_data['LDH'] if 'LDH' in request_data else None,
+            homocystein=request_data['homocystein'] if 'homocystein' in request_data else None)
+
+    diagnosis = get_diagnoses(examination)
+    examination.diagnosis = ', '.join(diagnosis)
+    examination.save()
 
 
-
-    return HttpResponse()
+    return HttpResponse(examination.diagnosis)
 
 
 
