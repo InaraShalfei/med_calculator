c76c0ef1e7d1a011d265c75232b8f17f37da147d
inara <inarashugaikina@gmail.com>
2023-07-21 11:32:27 +0500

    Change url

--- medical_calculator/calculator/urls.py
+++ medical_calculator/calculator/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('post/examinations', views.handle_results, name='handle_results'),
+    path('examinations', views.handle_results, name='handle_results'),
 ]
